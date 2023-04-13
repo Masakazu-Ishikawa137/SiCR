83c7e72f8a446fc2a4f418f3211ac47cd6bee3c4
Masakazu-Ishikawa137 <124025843+Masakazu-Ishikawa137@users.noreply.github.com>
2023-04-13 10:07:27 +0900

    deleted

--- Functions/230217script_second_functions_GEX.r
+++
@@ -1,4 +0,0 @@
-mysinglecell_metadata_for_UMAP <- function(seurat_object){
-    select(seurat_object@meta.data, !starts_with('TCR')) %>% select(!starts_with('BCR')) %>% select(-orig.ident, -nCount_RNA, -nFeature_RNA, -RNA_snn_res.0.5) %>% names() -> group
-    return(group)
-}
