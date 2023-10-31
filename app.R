82a554a8c82674bbdf1fd89e2607301f26557a4d
Masakazu-Ishikawa137 <124025843+Masakazu-Ishikawa137@users.noreply.github.com>
2023-10-31 14:23:37 +0900

    Bug Fixed

--- app.R
+++ app.R
@@ -26,68 +26,148 @@ ui <- navbarPage(
   tags$footer("© 2023 Your Company. All rights reserved."),
   
   tabPanel("Upload",
-    tabPanel("Cellranger output", uploadCellrangerUI("upload_cellranger"))
+    sidebarLayout(
+        uploadCellranger_sidebarUI('upload'),
+        uploadCellranger_mainUI('upload')
+    ),
   ),
   
   tabPanel("Gene Expression", 
     tabsetPanel(
-      tabPanel("UMAP", geneExpressionUmapUI("gene_expression_umap")),
-      tabPanel("Bar plot", geneExpressionBarplotUI("gene_expression_barplot"))
-    )
+      tabPanel(
+        "UMAP", 
+        sidebarLayout(
+          geneExpressionUmap_sidebarUI("gene_expression_umap"),
+          geneExpressionUmap_mainUI("gene_expression_umap")
+        ),
+      ),
+      tabPanel(
+        "Bar plot",
+        sidebarLayout(
+          geneExpressionBarplot_sidebarUI('gene_expression_barplot'),
+          geneExpressionBarplot_mainUI('gene_expression_barplot')
+        )
+      )
+    ),
   ),
   
-  tabPanel("TCR",
+  tabPanel(
+    "TCR",
     tabsetPanel(
-      tabPanel("Alpha diversity",    alphaDiversityUI("tcr_alpha_diversity")),
-      tabPanel("Clonal abundance",   clonalAbundanceUI("tcr_clonal_abundance")),
-      tabPanel("Clonotype expand",   clonotypeExpandUI("tcr_clonotype_expand")),
-      tabPanel("Gene usage",         geneUsageUI("tcr_gene_usage", chain_choice = list("TRA", "TRB"))),
-      tabPanel("Antigen prediction", antigenPredictionUI("tcr_antigen_prediction"))
-    )
+      tabPanel(
+        "Alpha diversity",
+        sidebarLayout(
+          alphaDiversity_sidebarUI("tcr_alpha_diversity"),
+          alphaDiversity_mainUI("tcr_alpha_diversity")),
+        ),
+      tabPanel(
+        "Clonal abundance",
+        sidebarLayout(
+          clonalAbundance_sidebarUI("tcr_clonal_abundance"),
+          clonalAbundance_mainUI("tcr_clonal_abundance"),
+        ),
+      ),
+      tabPanel(
+        "Clonotype expand",
+        sidebarLayout(
+          clonotypeExpand_sidebarUI("tcr_clonotype_expand"),
+          clonotypeExpand_mainUI("tcr_clonotype_expand")
+        ),
+      ),
+      tabPanel(
+        "Gene usage",
+        sidebarLayout(
+          geneUsage_sidebarUI("tcr_gene_usage", chain_choice = list("TRA", "TRB")),
+          geneUsage_mainUI("tcr_gene_usage", chain_choice = list("TRA", "TRB")),
+        ),
+      ),
+      tabPanel(
+        "Antigen prediction",
+        sidebarLayout(
+          antigenPrediction_sidebarUI("tcr_antigen_prediction"),
+          antigenPrediction_mainUI("tcr_antigen_prediction")
+        )
+      )
+    ),
   ),
   
   tabPanel("BCR",
     tabsetPanel(
-      tabPanel("Alpha diversity",    alphaDiversityUI("bcr_alpha_diversity")),
-      tabPanel("Clonal abundance",   clonalAbundanceUI("bcr_clonal_abundance")),
-      tabPanel("Clonotype expand",   clonotypeExpandUI("bcr_clonotype_expand")),
-      tabPanel("Gene usage",         geneUsageUI("bcr_gene_usage", chain_choice = list("IGH", "IGL"))),
-      tabPanel("Antigen prediction", antigenPredictionUI("bcr_antigen_prediction")),
-      tabPanel("Phylogenetic tree",  phylogeneticTreeUI("bcr_phylogenetic_tree"))
-    )
-  )
-  
+      tabPanel(
+        "Alpha diversity",
+        sidebarLayout(
+          alphaDiversity_sidebarUI("bcr_alpha_diversity"),
+          alphaDiversity_mainUI("bcr_alpha_diversity")
+        ),
+      ),
+      tabPanel(
+        "Clonal abundance",
+        sidebarLayout(
+          clonalAbundance_sidebarUI("bcr_clonal_abundance"),
+          clonalAbundance_mainUI("bcr_clonal_abundance")
+        ),
+      ),
+      tabPanel(
+        "Clonotype expand",
+        sidebarLayout(
+          clonotypeExpand_sidebarUI("bcr_clonotype_expand"),
+          clonotypeExpand_mainUI("bcr_clonotype_expand")
+        ),
+      ),
+      tabPanel(
+        "Gene usage",
+        sidebarLayout(
+          geneUsage_sidebarUI("bcr_gene_usage", chain_choice = list("IGH", "IGL")),
+          geneUsage_mainUI("bcr_gene_usage", chain_choice = list("IGH", "IGL")),
+        ),
+      ),
+      tabPanel(
+        "Antigen prediction",
+        sidebarLayout(
+          antigenPrediction_sidebarUI("bcr_antigen_prediction"),
+          antigenPrediction_mainUI("bcr_antigen_prediction")
+        ),
+      ),
+      tabPanel(
+        "Phylogenetic tree",
+        sidebarLayout(
+          phylogeneticTree_sidebarUI("bcr_phylogenetic_tree"),
+          phylogeneticTree_mainUI("bcr_phylogenetic_tree")
+        ),
+      ),
+    ),
+  ),
 )
 
 
 server <- function(input, output) {
   
-  dataList <- uploadCellrangerServer("upload_cellranger")
+  dataList <- uploadCellranger_mainServer("upload")
   
   seuratObject <- reactive({ dataList()[["seurat_object"]] })
   tcrList      <- reactive({ dataList()[["tcr_list"]] })
   bcrList      <- reactive({ dataList()[["bcr_list"]] })
   groupCols    <- reactive({ dataList()[["group_cols"]] })
   
-  geneExpressionUmapServer("gene_expression_umap", seuratObject(), groupCols())
-  geneExpressionBarplotServer("gene_expression_barplot", seuratObject()@meta.data, groupCols())
+  geneExpressionUmap_mainServer("gene_expression_umap", seuratObject(), groupCols())
+  geneExpressionBarplot_mainServer("gene_expression_barplot", seuratObject()@meta.data, groupCols())
   
-  alphaDiversityServer("tcr_alpha_diversity", tcrList()[["TRB"]], groupCols())
-  alphaDiversityServer("bcr_alpha_diversity", bcrList()[["IGH"]], groupCols())
+  alphaDiversity_mainServer("tcr_alpha_diversity", tcrList()[["TRB"]], groupCols())
+  alphaDiversity_mainServer("bcr_alpha_diversity", bcrList()[["IGH"]], groupCols())
   
-  clonalAbundanceServer("tcr_clonal_abundance", tcrList()[["TRB"]], groupCols())
-  clonalAbundanceServer("bcr_clonal_abundance", bcrList()[["IGH"]], groupCols())
+  clonalAbundance_mainServer("tcr_clonal_abundance", tcrList()[["TRB"]], groupCols())
+  clonalAbundance_mainServer("bcr_clonal_abundance", bcrList()[["IGH"]], groupCols())
   
-  clonotypeExpandServer("tcr_clonotype_expand", tcrList()[["TRB"]], groupCols())
-  clonotypeExpandServer("bcr_clonotype_expand", bcrList()[["IGH"]], groupCols())
+  clonotypeExpand_mainServer("tcr_clonotype_expand", tcrList()[["TRB"]], groupCols())
+  clonotypeExpand_mainServer("bcr_clonotype_expand", bcrList()[["IGH"]], groupCols())
   
-  geneUsageServer("tcr_gene_usage", tcrList(), groupCols())
-  geneUsageServer("bcr_gene_usage", bcrList(), groupCols())
+  geneUsage_mainServer("tcr_gene_usage", tcrList(), groupCols())
+  geneUsage_mainServer("bcr_gene_usage", bcrList(), groupCols())
   
-  antigenPredictionServer("tcr_antigen_prediction", tcrList()[["TRB"]], "data/230323_ruft_TCR_antigen_database.tsv")
-  antigenPredictionServer("bcr_antigen_prediction", bcrList()[["IGH"]], "data/230323_ruft_BCR_antigen_database.tsv")
+  antigenPrediction_mainServer("tcr_antigen_prediction", tcrList()[["TRB"]], "data/230323_ruft_TCR_antigen_database.tsv")
+  antigenPrediction_mainServer("bcr_antigen_prediction", bcrList()[["IGH"]], "data/230323_ruft_BCR_antigen_database.tsv")
   
-  phylogeneticTreeServer("bcr_phylogenetic_tree", bcrList()[["IGH"]])
+  phylogeneticTree_mainServer("bcr_phylogenetic_tree", bcrList()[["IGH"]])
   
 }
 
